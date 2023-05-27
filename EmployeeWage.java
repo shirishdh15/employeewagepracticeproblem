2691b322e6646be7d6e8daa929f4a317346d6066
shirishdh15 <121274610+shirishdh15@users.noreply.github.com>
2023-05-27 16:53:37 +0530

    Add files via upload

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,16 +3,20 @@ package practiceProblems;
 import java.util.Random;
 
 public class EmployeeWage {
-        public static void main(String[] args) {
-            System.out.println("Welcome to Employee Wage Computation");
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
 
-            Random attendance =new Random();
-            int a = attendance.nextInt(2);
-            if( a == 0) {
-                System.out.println("employee is absent");
-            }
-            else {
-                System.out.println("employee is present");
-            }
+        Random attendance = new Random();
+        int a = attendance.nextInt(2);
+        if (a == 0) {
+            System.out.println("employee is absent");
+        } else {
+            System.out.println("employee is present");
+
+            int wagePerHour = 20;
+            int fullDay = 8;
+            int dailyWage = (wagePerHour * fullDay);
+            System.out.println("Daily employee Wage is  " + dailyWage);
         }
     }
+}
